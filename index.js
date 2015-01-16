81b8f212f6711f434f0324a9234cb783fc5cf185
Mert Kahyaoğlu <mertkahyaoglu93@gmail.com>
2015-01-17 01:39:52 +0200

    error handling
    if response has error which means there is no movie with that random id,
    request with a new random id

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ var movie = require('node-movie');
 var rand = require('unique-random')(0,2900000);
 var leadingZeros = require('leading-zeros');
 
-module.exports = function (cb){
+module.exports = function again(cb){
 	var randNum = rand();
 	var randStr = randNum + '';
 	var randID = 'tt' + leadingZeros(randNum, 7-randStr.length);
     movie({id: randID}, function(data) {
+			//No movie with this random ID.
+			if(data.Error) {
+				again(cb);
+			}
       cb(data);
     });
 };
